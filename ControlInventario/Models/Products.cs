d9201eb1c44f89cbd442eebab4d380fbd9370297
Villegas Ramirez Luis Eduardo <luis.villegas17@tectijuana.edu.mx>
2020-05-11 11:48:09 -0700

    Dashboard

---
+++ ControlInventario/Models/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ControlInventario.Models
+{
+    public class Products
+    {
+        public int Id { get; set; }
+        public int ProductsCode { get; set; }
+        public string ProductsName { get; set; }
+        public String Description { get; set; }
+        public int Quantity { get; set; }
+        public string Price { get; set; }
+        public int SupplierId { get; set; }
+        [ForeignKey("SupplierId")]
+        public Supplier Supplier { get; set; }
+    }
+}
